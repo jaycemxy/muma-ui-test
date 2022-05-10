79ecdd6c81b2be6bab83a0cddcf8946b64a66d27
joyce <593205312@qq.com>
2022-05-10 15:53:43 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hello</div>
+  <Joyce/>
+</template>
+
+<script>
+import Joyce from './components/Joyce.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Joyce
+  }
+}
+</script>
