cdf5dbab2f7f81b09a9acce604c46f86776c6012
joyce <593205312@qq.com>
2022-05-11 14:03:24 +0800

    路由切换后隐藏aside

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
 
 <script lang="ts">
 import { ref, provide } from 'vue'
+import { router } from './router'
 
 export default {
   name: 'App',
@@ -11,6 +12,11 @@ export default {
     const width = document.documentElement.clientWidth;
     const asideVisible = ref(width <= 500 ? false:true)  // 通过获取页面宽度，决定 asideVisible变量 的初始值为true或false
     provide('asideVisible', asideVisible)
+    router.afterEach(()=>{
+      if( width <= 500 ){
+        asideVisible.value = false
+      }
+    })
   }
 }
 </script>
