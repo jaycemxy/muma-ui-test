f6f817074cc246542ffef3ec6d5ad65806ac2d82
joyce <593205312@qq.com>
2022-05-12 16:36:37 +0800

    使用v-model双向绑定

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,13 +1,18 @@
 <template>
     <div>
-        <Switch />
+        <Switch v-model:value="y" />
     </div>
 </template>
 
 <script lang="ts">
+import { ref } from 'vue'
 import Switch from '../lib/Switch.vue'
 
 export default {
-    components: {Switch}
+    components: {Switch},
+    setup(){
+        const y = ref(false)
+        return {y}
+    }
 }
 </script>
