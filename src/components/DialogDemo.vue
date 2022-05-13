d9c1dd83afe7f20fbf239c0d0d4622d572deb333
joyce <593205312@qq.com>
2022-05-13 21:43:17 +0800

    Dialog 支持title和content

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -2,7 +2,10 @@
     <div>Dialog 示例</div>
     <h1>示例1</h1>
     <Button @click="toggle">toggle</Button>
-    <Dialog v-model:visible="x" :closeOnClickOverlay="false" :ok="f1" :cancel="f2"></Dialog>
+    <Dialog v-model:visible="x" :closeOnClickOverlay="false" :ok="f1" :cancel="f2">
+        <div>hi</div>
+        <div>hi222</div>
+    </Dialog>
 </template>
 
 <script lang="ts">
--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -3,10 +3,12 @@
         <div class="gulu-dialog-overlay" @click="onClickOverlay"></div>
         <div class="gulu-dialog-wrapper">
             <div class="gulu-dialog">
-                <header>标题<span @click="close" class="gulu-dialog-close"></span></header>
+                <header>
+                    {{title}}
+                    <span @click="close" class="gulu-dialog-close"></span>
+                </header>
                 <main>
-                    <p>第一行字</p>
-                    <p>第二行字</p>
+                    <slot/>
                 </main>
                 <footer>
                     <Button @click="ok">OK</Button>
@@ -22,6 +24,10 @@ import Button from './Button.vue'
 
 export default {
     props: {
+        title: {
+            type: String,
+            default: '自定义标题'
+        },
         visible: {
             type: Boolean,
             default: false
