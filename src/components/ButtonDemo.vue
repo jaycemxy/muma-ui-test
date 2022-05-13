19bcd0674f70e0d362dc6c088b94c55aca0b4e6b
joyce <593205312@qq.com>
2022-05-13 10:04:59 +0800

    创建 Button组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,5 +1,13 @@
 <template>
+    <div>button示例</div>
     <div>
-        Button 的文档
+        <Button>(*´▽｀)你好</Button>
     </div>
 </template>
+
+<script lang="ts">
+    import Button from '../lib/Button.vue'
+    export default {
+        components: {Button}
+    }
+</script>
