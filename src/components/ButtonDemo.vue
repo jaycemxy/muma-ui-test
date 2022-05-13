1ad269b6b49e75a4d6e6a587ca3483cbda463af4
joyce <593205312@qq.com>
2022-05-13 12:15:25 +0800

    css最小影响原则

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,10 +1,10 @@
 <template>
     <div>button示例</div>
     <div>
-        <Button>(*´▽｀)你好</Button>
-        <Button theme="button">(*´▽｀)你好</Button>
-        <Button theme="link">(*´▽｀)你好</Button>
-        <Button theme="text">(*´▽｀)你好</Button>
+        <Button>你好</Button>
+        <Button theme="button">你好</Button>
+        <Button theme="link">你好</Button>
+        <Button theme="text">你好</Button>
     </div>
 </template>
 
