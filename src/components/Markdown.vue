b0fb84449b799bc3e6d03cd6e80b3c44a2a7603e
joyce <593205312@qq.com>
2022-05-23 15:13:55 +0800

    抽出markdown模板

--- src/components/Markdown.vue
+++ src/components/Markdown.vue
@@ -1,17 +1,23 @@
 <template>
-    <article class="markdown-body" v-html="md"></article>
+    <article class="markdown-body" v-html="content"></article>
 </template>
 
 <script lang="ts">
-    export default {
-        props: {
-            path: {
-                type: String,
-                required: true
-            }
-        },
-        setup(props){
-            import(props.path)
+import {ref} from 'vue'
+
+export default {
+    props: {
+        path: {
+            type: String,
+            required: true
         }
+    },
+    setup(props){
+        const content = ref<string>(null)
+        import(props.path).then(result=>{  // 动态import，异步获取结果
+            content.value = result.default
+        })
+        return {content}
     }
+}
 </script>
--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -1,18 +1,11 @@
 <template>
-  <article class="markdown-body" v-html="md">
-  </article>
-
-  <!-- <Markdown path="../markdown/intro.md"/> -->
+  <Markdown path="../markdown/intro.md"/>
 </template>
 
 <script>
-import md from '../markdown/intro.md'
-// import Markdown from '../components/Markdown.vue'
+import Markdown from '../components/Markdown.vue'
 
 export default {
-  // components: {Markdown},
-  data(){
-      return {md}
-  }
+  components: {Markdown}
 }
 </script>
