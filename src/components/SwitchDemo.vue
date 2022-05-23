44ac3e528c1fa4545384007513da41bad2b0434e
joyce <593205312@qq.com>
2022-05-23 23:43:29 +0800

    展示源码

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -10,7 +10,7 @@
                 <Button>查看代码</Button>
             </div>
             <div class="demo-code">
-                <pre>&lt;Switch v-model:value="bool" /&gt;</pre>
+                <pre>{{Switch1Demo.__sourceCode}}</pre>
             </div>
         </div>
         <div class="demo">
@@ -22,7 +22,7 @@
                 <Button>查看代码</Button>
             </div>
             <div class="demo-code">
-                <pre>&lt;Switch v-model:value="bool" disabled /&gt;</pre>
+                <pre>{{Switch2Demo.__sourceCode}}</pre>
             </div>
         </div>
     </div>
@@ -35,11 +35,16 @@ import Button from '../lib/Button.vue'
 import Switch1Demo from '../components/Switch1.demo.vue'
 import Switch2Demo from '../components/Switch2.demo.vue'
 
+console.log(Switch1Demo.__sourceCode)  // 组件.__sourceCode 展示源码
+console.log(Switch1Demo.__sourceCodeTitle)
+
 export default {
     components: {Switch, Button, Switch1Demo, Switch2Demo},
     setup(){
         const bool = ref(false)
-        return {bool}
+        return {
+            bool, Switch1Demo, Switch2Demo
+        }
     }
 }
 </script>
