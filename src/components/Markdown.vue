d04bdfb5bf73c7715041e2a5951597430dd14303
joyce <593205312@qq.com>
2022-05-24 16:20:50 +0800

    解决 build 后不加载md文件的问题

--- src/components/Markdown.vue
+++ src/components/Markdown.vue
@@ -3,21 +3,13 @@
 </template>
 
 <script lang="ts">
-import {ref} from 'vue'
 
 export default {
     props: {
-        path: {
+        content: {
             type: String,
             required: true
         }
-    },
-    setup(props){
-        const content = ref<string>(null)
-        import(props.path).then(result=>{  // 动态import，异步获取结果
-            content.value = result.default
-        })
-        return {content}
     }
 }
 </script>
